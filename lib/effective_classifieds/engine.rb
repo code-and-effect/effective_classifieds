b11e067b3ef1ab14fcaef3fe31a0f731c616f541
Matt Riemer <matthew@codeandeffect.com>
2023-12-04 10:15:05 -0700

    Use rails config.to_prepare

--- lib/effective_classifieds/engine.rb
+++ lib/effective_classifieds/engine.rb
@@ -9,7 +9,7 @@ module EffectiveClassifieds
 
     # Include acts_as_addressable concern and allow any ActiveRecord object to call it
     initializer 'effective_classifieds.active_record' do |app|
-      ActiveSupport.on_load :active_record do
+      app.config.to_prepare do
         ActiveRecord::Base.extend(EffectiveClassifiedsClassifiedWizard::Base)
       end
     end
